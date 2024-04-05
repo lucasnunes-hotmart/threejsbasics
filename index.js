b2e5e99861363065aefd8cb5bf87261001ad0cce
Lucas Nunes <96444035+lucasnunes-hotmart@users.noreply.github.com>
2024-04-05 19:52:06 +0000

    fix

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-const controls = new OrbitControls( camera, renderer.domElement );
-
 // Criar renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -21,6 +19,8 @@ var geometry = new THREE.BoxGeometry();
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const controls = new OrbitControls( camera, renderer.domElement );
 
 controls.update();
 
