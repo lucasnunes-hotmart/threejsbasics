aded4c005a6bbfeaffae8ac62c31972898d5c9ce
Lucas Nunes <96444035+lucasnunes-hotmart@users.noreply.github.com>
2024-04-05 19:47:56 +0000

    teste

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ function animate() {
 }
 
 function init() {
+  createPanel()
   animate();
 }
 
@@ -48,79 +49,7 @@ function createPanel() {
   const panel = new GUI( { width: 310 } );
 
   const folder1 = panel.addFolder( 'Base Actions' );
-  const folder2 = panel.addFolder( 'Additive Action Weights' );
-  const folder3 = panel.addFolder( 'General Speed' );
-
-  panelSettings = {
-    'modify time scale': 1.0
-  };
-
-  const baseNames = [ 'None', ...Object.keys( baseActions ) ];
-
-  for ( let i = 0, l = baseNames.length; i !== l; ++ i ) {
-
-    const name = baseNames[ i ];
-    const settings = baseActions[ name ];
-    panelSettings[ name ] = function () {
-
-      const currentSettings = baseActions[ currentBaseAction ];
-      const currentAction = currentSettings ? currentSettings.action : null;
-      const action = settings ? settings.action : null;
-
-      if ( currentAction !== action ) {
-
-        prepareCrossFade( currentAction, action, 0.35 );
-
-      }
-
-    };
-
-    crossFadeControls.push( folder1.add( panelSettings, name ) );
-
-  }
-
-  for ( const name of Object.keys( additiveActions ) ) {
-
-    const settings = additiveActions[ name ];
-
-    panelSettings[ name ] = settings.weight;
-    folder2.add( panelSettings, name, 0.0, 1.0, 0.01 ).listen().onChange( function ( weight ) {
-
-      setWeight( settings.action, weight );
-      settings.weight = weight;
-
-    } );
-
-  }
-
-  folder3.add( panelSettings, 'modify time scale', 0.0, 1.5, 0.01 ).onChange( modifyTimeScale );
 
   folder1.open();
-  folder2.open();
-  folder3.open();
-
-  crossFadeControls.forEach( function ( control ) {
-
-    control.setInactive = function () {
-
-      control.domElement.classList.add( 'control-inactive' );
-
-    };
-
-    control.setActive = function () {
-
-      control.domElement.classList.remove( 'control-inactive' );
-
-    };
-
-    const settings = baseActions[ control.property ];
-
-    if ( ! settings || ! settings.weight ) {
-
-      control.setInactive();
-
-    }
-
-  } );
 
 }
