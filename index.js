833232f2384da2eb81f63c5c0106eda23b833c50
Lucas Nunes <96444035+lucasnunes-hotmart@users.noreply.github.com>
2024-04-19 18:25:31 +0000

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 
 // Criar cena
 var scene = new THREE.Scene();
+scene.background = 'white'
 
 // Criar câmera
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -15,13 +16,13 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Criar cubo
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 'white' });
+var geometry = new THREE.BoxGeometry(2,2,2);
+var material = new THREE.MeshBasicMaterial({ color: 'red' });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 scene.add(cube);
 
-const baseParams = { rotationSpeed: 0.01, width: '50px', height: '50px' }
+const baseParams = { rotationSpeed: 0.01, width: 2, height: 2, depth: 2 }
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
@@ -36,6 +37,9 @@ function animate() {
   // Girar o cubo
   cube.rotation.x += baseParams.rotationSpeed;
   cube.rotation.y += baseParams.rotationSpeed;
+  cube.width = baseParams.width
+  cube.height = baseParams.height
+  cube.depth = baseParams.depth
 
   controls.update();
 
@@ -55,6 +59,7 @@ function createPanel() {
   folder1.add(baseParams, 'rotationSpeed', 0, 0.1)
   folder1.add(baseParams, 'width', 10, 200)
   folder1.add(baseParams, 'height', 10, 200)
+  folder1.add(baseParams, 'depth', 10, 200)
 
   folder1.open();
 
